58a4c99eb127d547e6cf4ecc75d7493bbc2e8167
zzl <zzl@zzl.com>
2024-11-28 14:24:51 +0800

    hot-fix commit 2

--- src/main/java/com/zzl/Git.java
+++ src/main/java/com/zzl/Git.java
@@ -7,5 +7,6 @@ public class Git {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hot-fix test");
     }
 }
