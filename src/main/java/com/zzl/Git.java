9da927edb85dd4b3419293f911b31d456dc6bf75
zzl-0902 <2010210316@qq.com>
2024-11-28 14:49:29 +0800

    pull commit

--- src/main/java/com/zzl/Git.java
+++ src/main/java/com/zzl/Git.java
@@ -10,5 +10,6 @@ public class Git {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
