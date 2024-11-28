0e56e5b846a4df8375bf9be1a1b8dcfec1fe65e1
zzl <zzl@zzl.com>
2024-11-28 14:43:03 +0800

    push commit 2

--- src/main/java/com/zzl/Git.java
+++ src/main/java/com/zzl/Git.java
@@ -9,5 +9,6 @@ public class Git {
         System.out.println("hello git4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
